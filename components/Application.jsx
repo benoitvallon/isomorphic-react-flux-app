2e686e9d9d7db23dbaf6a5e0fc7c5ff17216aab6
Benoit VALLON <benoitvallon@gmail.com>
2014-12-23 12:04:14 +0100

    Adds the application react component

---
+++ components/Application.jsx
@@ -0,0 +1,23 @@
+'use strict';
+
+var React = require('react');
+var ApplicationStore = require('../stores/ApplicationStore');
+var Home = require('./Home.jsx');
+var About = require('./About.jsx');
+var StoreMixin = require('fluxible-app').StoreMixin;
+
+var Application = React.createClass({
+  mixins: [StoreMixin],
+  getInitialState: function () {
+    return this.getStore(ApplicationStore).getState();
+  },
+  render: function(){
+    return (
+      <div>
+        {'home' === this.state.currentPageName ? <Home/> : <About/>}
+      </div>
+    );
+  }
+});
+
+module.exports = Application;
